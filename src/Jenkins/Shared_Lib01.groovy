666e2bb6f6220ae9916478047013e155f1b11ce1
Joganath <joganath.sahoo@gmail.com>
2020-03-10 10:21:41 +0530

    Groovy shared library class implementation

---
+++ src/Jenkins/Shared_Lib01.groovy
@@ -0,0 +1,20 @@
+package Jenkins;
+
+public class Shared_Lib01 {
+
+
+def Code_Checkout()
+{
+	chckout scm
+}
+def Cleanup(String Dir)
+{
+	sh 'rm -r Dir'
+}
+
+def Build()
+{
+	sh 'mvn clean install'
+}
+
+}
