f034d6738a7a0222ddbe229591a12fb6306b14da
Joganath <joganath.sahoo@gmail.com>
2020-03-15 20:57:15 +0530

    Grab annotation added

--- src/Shared_Lib01.groovy
+++ src/Shared_Lib01.groovy
@@ -1,17 +1,20 @@
 package jenkins.sharedlib
+import org.apache.commons.lang3.SystemUtils
 
+@Grab('org.apache.commons:commons-lang3:3.8.1')
 class Shared_Lib01 {
 
 def Cleanup(String dir)
 {
-	steps.sh 'rm -r dir'
+    steps.sh 'rm -r dir'
+    
 
-	}
+    }
 
 def Build()
 {
-	steps.sh "mvn clean install"
+    steps.sh "mvn clean install"
 
-	}
+    }
 
 }
