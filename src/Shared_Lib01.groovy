c9a4cf5a8e67fe4b7cd501f8c3084b28ab1141b0
Joganath <joganath.sahoo@gmail.com>
2020-03-15 21:12:25 +0530

    parameterized constructor added in class

--- src/Shared_Lib01.groovy
+++ src/Shared_Lib01.groovy
@@ -3,6 +3,11 @@ import org.apache.commons.lang3.SystemUtils
 
 @Grab('org.apache.commons:commons-lang3:3.8.1')
 class Shared_Lib01 {
+def steps
+Shared_Lib01(steps)
+{
+this.steps=steps
+}
 
 def Cleanup(String dir)
 {
