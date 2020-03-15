8c02b1ec5aa18586949fb3996f8dc10be362d228
Joganath <joganath.sahoo@gmail.com>
2020-03-15 20:45:34 +0530

    steps prefixed with sh command

--- src/Shared_Lib01.groovy
+++ src/Shared_Lib01.groovy
@@ -2,21 +2,16 @@ package jenkins.sharedlib
 
 class Shared_Lib01 {
 
-
-def Code_Checkout()
-{
-	chckout scm
-}
 def Cleanup(String dir)
 {
-	sh 'rm -r dir'
-return this
+	steps.sh 'rm -r dir'
+
 	}
 
 def Build()
 {
-	sh "mvn clean install"
-return this
+	steps.sh "mvn clean install"
+
 	}
 
 }
